1653162e4a5b8c0e0e6c36b18fcf13fa6a7d8292
KappelM <magnus.kappel@icloud.com>
2024-10-22 11:03:49 +0200

    Gamemanager update
    Added several scripts that the gamemanger will probably use

---
+++ Elemental-Odyssey/Assets/Scripts/SceneTransistionManager.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTransitionManager : MonoBehaviour
+{
+    public void SwitchScene(string sceneName)
+    {
+        throw new NotImplementedException();
+    }
+}
