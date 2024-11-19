87effec6fda9a9d6974ba6fecd0801ac94a19d20
MarcusStorvangJonassen <95277125+MarcusStorvangJonassen@users.noreply.github.com>
2024-11-19 15:19:33 +0100

    Done

--- Elemental-Odyssey/Assets/Pickupscript.cs
+++ Elemental-Odyssey/Assets/Pickupscript.cs
@@ -7,9 +7,9 @@ public class Pickupscript : MonoBehaviour
 {
     public GameManager GameManager;
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(collision.CompareTag("Key") || collision.CompareTag("Potion")) GameManager.handlepickup(collision.name);
-        Destroy(collision.gameObject);
-    }
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if(collision.CompareTag("Key") || collision.CompareTag("Potion")) GameManager.handlepickup(collision.name);
+    //    Destroy(collision.gameObject);
+    //}
 }
