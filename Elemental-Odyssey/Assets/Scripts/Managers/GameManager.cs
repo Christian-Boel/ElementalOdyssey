3ef485e60b3d1253e6825cd52df847de90f75537
Christian-Boel <104983283+Christian-Boel@users.noreply.github.com>
2024-11-26 14:53:11 +0100

    Added locked door prefab

--- Elemental-Odyssey/Assets/Scripts/Managers/GameManager.cs
+++ Elemental-Odyssey/Assets/Scripts/Managers/GameManager.cs
@@ -10,15 +10,16 @@ public class GameManager : MonoBehaviour
     
     private GameObject _player;
     public GameObject _playerPrefab;
-    
+
+    [SerializeField] private AudioClip healthPotionSoundClip;
+    [SerializeField] private AudioClip keySoundClip;
     private string _currentSceneName;
     private bool _shouldSpawnPlayer = false;
     private PlayerStats _playerStats;
     private AudioManager audioManager;
     private SceneTransitionManager sceneTransitionManager;
     private HashSet<KeyType> _collectedKeys = new HashSet<KeyType>();
-
-
+    
     void Awake()
     {
         if (Instance == null)
@@ -85,12 +86,14 @@ public class GameManager : MonoBehaviour
     {
         int healAmount = item.healAmount;
         _playerStats.Heal(healAmount);
+        SoundFXManager.instance.PlaySoundFXClip(healthPotionSoundClip,transform, 1f);
         Debug.Log("Picked up a health potion. Healed for " + healAmount);
     }
 
     private void HandleKey(Item item)
     {
         KeyType keyType = item.keyType;
+        SoundFXManager.instance.PlaySoundFXClip(keySoundClip,transform, 1f);
         AddKey(keyType);
         Debug.Log("Picked up a key: " + keyType);
     }
