f07851b389c746ef8be0d58b5e44a73934e1edb1
Christian-Boel <104983283+Christian-Boel@users.noreply.github.com>
2024-12-03 13:52:21 +0100

    hud nice

--- Elemental-Odyssey/Assets/Scripts/PlayerStatsUI.cs
+++ Elemental-Odyssey/Assets/Scripts/PlayerStatsUI.cs
@@ -1,18 +1,29 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerStatsUI : MonoBehaviour
 {
-    public PlayerStats playerStats; // Reference to the PlayerStats script
-    public Text playerStatsText; // Reference to the UI Text element
+    public TextMeshProUGUI playerStatsText; // Reference to the UI Text element for displaying stats
+    private PlayerStats playerStats; // Reference to the PlayerStats script
 
     void Update()
     {
+        if (!playerStats)
+        {
+            playerStats = GameManager.Instance.GetComponent<PlayerStats>(); 
+        }
         UpdateStatsDisplay();
     }
-
+    
+    
     private void UpdateStatsDisplay()
     {
-        playerStatsText.text = $"Health: {playerStats.currentHealth}/{playerStats.maxHealth}";
+        playerStatsText.text = 
+            $"Health: {playerStats.currentHealth}/{playerStats.maxHealth}\n" +
+            $"Attack Damage: {playerStats.AD}\n" +
+            $"Movement Speed: {playerStats.MS}\n" +
+            $"Dash Cooldown: {playerStats.dashCD}\n" +
+            $"Dash Length: {playerStats.dashLength}";
     }
 } 
